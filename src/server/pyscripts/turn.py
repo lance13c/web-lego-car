ad69728ca6ad0260ef1efa4b60453d9f00c1e137
root <pianoman13c@gmail.com>
2016-12-05 03:35:06 +0000

    turn python script to turn servo

--- src/server/pyscripts/turn.py
+++ src/server/pyscripts/turn.py
@@ -1,5 +1,35 @@
-#Python 2.x
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
+import sys
 
-GPIO.setmode(GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(19, GPIO.OUT)
+pwm = GPIO.PWM(19, 100)
+pwm.start(5)
+
+count = 0
+on = True
+
+#Get the Angle from Arguments - First Arg
+#angleArg = int(sys.argv[1])
+
+
+def turn(angle):
+    if (angle > 180):
+        angle = 180
+        
+    if (angle < 0):
+        angle = 0
+    duty = float(angle) / 10.0 + 2.5
+    pwm.ChangeDutyCycle(duty)
+    print(angle)
+    time.sleep(1)
+
+# Loops awaiting turning inputs
+while (on == True):
+    data = input()              # either false or angle(integer)
+    if (str(data) == "false"):
+        on = False
+    else:
+        turn(data)
