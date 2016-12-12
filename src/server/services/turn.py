932f38da296789f70d0fe34213c34efba65fd3d9
lance13c <pianoman13c@gmail.com>
2016-12-12 06:53:16 -0500

    update docs

--- src/server/services/turn.py
+++ src/server/services/turn.py
@@ -1,3 +1,4 @@
+# Reused code from https://github.com/miguelgrinberg/python-socketio
 import socketio
 import RPi.GPIO as GPIO
 import eventlet
@@ -5,12 +6,15 @@ import eventlet.wsgi
 from flask import Flask, render_template
 import time
 
+# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(19, GPIO.OUT)
 pwm = GPIO.PWM(19, 100)
 pwm.start(5)
 
+# Updates the servo to the specified angle
+# angle - Number from 0-180
 def update(angle):
     duty = float(angle) / 10.0 + 2.5
     pwm.ChangeDutyCycle(duty)
@@ -20,15 +24,18 @@ sio = socketio.Server()
 PORT = 8080
 app = Flask(__name__)
 
+# Host non existent index.html
 @app.route('/')
 def index():
     """Serve the client-side application."""
     return render_template('index.html')
 
+# Do this when a socket.io client connects
 @sio.on('connect', namespace='/servo')
 def connect(sid, environ):
     print("connect ", sid)
 
+# Do this when a turn command is sent to this socket.io server
 @sio.on('turn', namespace='/servo')
 def message(sid, data):
     data = str(data)
@@ -41,10 +48,12 @@ def message(sid, data):
         
     sio.emit('reply:', room=sid)
 
+# Do this when a socket.io client disconnects
 @sio.on('disconnect', namespace='/servo')
 def disconnect(sid):
     print('disconnect ', sid)
 
+# Starts the socket.io server
 if __name__ == '__main__':
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
