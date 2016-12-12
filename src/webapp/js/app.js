382950cd361600d5c194e42504654fe8d0dbe6b9
lance13c <pianoman13c@gmail.com>
2016-12-12 06:54:50 -0500

    update docs

--- src/webapp/js/app.js
+++ src/webapp/js/app.js
@@ -2,9 +2,8 @@
  * Created by dpc20 on 11/26/2016.
  */
 () => {
-
-
-    // Event Handlers
+	
+    // Event Handlers - prevents random scrolling
 		body.addEventListener((e) => {
 			e.preventDefault();
 		});
