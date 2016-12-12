2e2cea0f796bc2b3bfbcbdfc7eab34f2be1a7c58
lance13c <pianoman13c@gmail.com>
2016-12-12 06:37:05 -0500

    fix tests change from turn to stop

--- spec/rest/vehicle/stopSpec.js
+++ spec/rest/vehicle/stopSpec.js
@@ -3,11 +3,10 @@
  */
 const unirest = require('unirest');
 
-describe('turn', () => {
+describe('stop', () => {
     it('should return 200 status', function (done) {
         unirest.post('http://127.0.0.1:3000/vehicle/stop')
             .end(function(res) {
-                //console.log(`Error: ${res.error}`);
                 expect(res.error).toBe(false);
                 expect(res.status).toEqual(200);
                 done();
